c2f73289851aed24f0153c6e2acc9aa2f21150c8
sugrace <sugrace3000@gmail.com>
2020-04-15 16:08:48 +0900

    adds Weather.js?

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from "./Weather";
+
 
 export default function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      setIsLoaded(true);
+      console.log(position);
+      },
+      (error)=>{ setError(error); }
+      )
+  },[])
+
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar hidden={true}/>
+      {isLoaded ? <Weather/> : (
+      <View style={styles.loading}>
+        <Text style={styles.loadingText}>Getting the Weather</Text>
+        {error ? <Text style={styles.errorText}>{error}</Text>: null}        
+        </View>
+      )}
     </View>
+    </>
   );
 }
 
@@ -13,7 +34,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  errorText:{
+    color:"red",
+    backgroundColor:"transparent",
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 23,
+  },
+  loadingText:{
+    fontSize: 35,
+    marginBottom: 100,
+  }
 });
